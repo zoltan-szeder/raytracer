fdf348ed38af6c85f4da5e4f20d60ec1b0c1d46d
Zoltan Szeder <zoltan.szeder@tessarine.co.uk>
2024-04-13 19:10:03 +0100

    Extract Camera.hpp

--- Camera.cpp
+++ Camera.cpp
@@ -1,81 +1,66 @@
-struct Camera {
-private:
-  Vector pos;
-  Vector focus;
-  Vector distance;
-  Vector up;
-  Vector right;
-  Screen* screen;
+#include "Camera.hpp"
 
-  Model* model;
+Camera::Camera(Model* model0, const Vector& pos0, const Vector& focus0, Screen* screen0) {
+  model = model0;
+  pos = pos0;
+  focus = focus0;
+  screen = screen0;
 
-public:
-  Camera( Model* model0, const Vector& pos0, const Vector& focus0, Screen* screen0 )
+  Vector v = (focus - pos).normalize();
+
+  if(v.px == 0 && v.py == 0)
   {
-    model = model0;
-    pos = pos0;
-    focus = focus0;
-    screen = screen0;
-
-    Vector v = ( focus - pos ).normalize();
-
-    if( v.px == 0 && v.py == 0 )
+    if(v.pz > 0)
     {
-      if( v.pz > 0 )
-      {
-        up = Vector( 0, 0, 1 );
-        right = Vector( 0, 1, 0 );
-      }
-      else
-      {
-        up = Vector( 0, 0, -1 );
-        right = Vector( 0, -1, 0 );
-      }
+      up = Vector(0, 0, 1);
+      right = Vector(0, 1, 0);
     }
-    else {
-      float l = -( v.pz / ( v * v ) );
-
-      up = ( Vector( 0, 0, 1 ) + v*l ).normalize();
-      right = v % up;
-    }
-
-    distance = v*1.5;
-  }
-
-  void render()
-  {
-    Vector i = up / ( screen->y / 2.0 );
-    Vector j = right / ( screen->y / 2.0 );
-    int py = 0;
-    for( int ly = -screen->y / 2; ly < screen->y / 2; ly++ )
+    else
     {
-      int px = 0;
-      for( int lx = -screen->x / 2; lx < screen->x / 2; lx++)
-      {
-        Ray r( pos, distance + i*ly + j*lx );
-        Color c = trace( &r );
-
-				screen->canvas[py*screen->y + px] = c;
-        px++;
-      }
-      py++;
-      screen->draw();
+      up = Vector(0, 0, -1);
+      right = Vector(0, -1, 0);
     }
   }
+  else {
+    float l = -(v.pz / (v * v));
 
-  Color trace( Ray* r )
-  {
-    Object* o[1];
-    o[0] = NULL;
-    Intersection in = model->intersect( r, o );
-
-    if( *o )
-    {
-      Light** l = model->getLights();
-      int s = model->getLightsNum();
-      Material* m = (*o)->getMaterial();
-      return m->bounce( in, l, s );
-    }
-    return Color(0.694, 0.914, 0.984);
+    up = (Vector(0, 0, 1) + v*l).normalize();
+    right = v % up;
   }
-};
+
+  distance = v*1.5;
+}
+
+void Camera::render() {
+  Vector i = up / (screen->y / 2.0);
+  Vector j = right / (screen->y / 2.0);
+  int py = 0;
+  for(int ly = -screen->y / 2; ly < screen->y / 2; ly++) {
+    int px = 0;
+    for(int lx = -screen->x / 2; lx < screen->x / 2; lx++) {
+      Ray r(pos, distance + i*ly + j*lx);
+      Color c = trace(&r);
+
+        screen->canvas[py*screen->y + px] = c;
+      px++;
+    }
+    py++;
+    screen->draw();
+  }
+}
+
+Color Camera::trace(Ray* r) {
+  Object* o[1];
+  o[0] = NULL;
+  Intersection in = model->intersect(r, o);
+
+  if(*o) {
+    Light** l = model->getLights();
+    int s = model->getLightsNum();
+    Material* m = (*o)->getMaterial();
+    return m->bounce(in, l, s);
+  }
+
+  return Color(0.694, 0.914, 0.984);
+}
+
---
+++ Camera.hpp
@@ -0,0 +1,23 @@
+#include "Model.hpp"
+#include "Vector.hpp"
+#include "Screen.hpp"
+#include "Color.hpp"
+
+struct Camera {
+private:
+  Vector pos;
+  Vector focus;
+  Vector distance;
+  Vector up;
+  Vector right;
+  Screen* screen;
+
+  Model* model;
+
+public:
+  Camera( Model* model0, const Vector& pos0, const Vector& focus0, Screen* screen0 );
+
+  void render();
+
+  Color trace( Ray* r );
+};
