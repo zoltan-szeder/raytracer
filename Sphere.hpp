ca6f41d2d13f4577adf48a77d2774f51541f2459
Zoltan Szeder <zoltan.szeder@tessarine.co.uk>
2024-04-21 20:42:17 +0100

    Extract Sphere.hpp

---
+++ Sphere.hpp
@@ -0,0 +1,18 @@
+#ifndef SPHERE_HPP
+#define SPHERE_HPP
+
+#include "Vector.hpp"
+#include "Material.hpp"
+#include "Intersection.hpp"
+
+
+struct Sphere : Object {
+public:
+  float rad;
+
+  Sphere(Vector* p0, Material* m0, float rad0 = 1);
+  Intersection intersect(Ray* r);
+  void createChild(Sphere* sphere);
+};
+
+#endif
